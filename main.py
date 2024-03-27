848daaa61835f7c67717a6810396c26e0159ad90
Дмитрий Большаков <bolshakovdmitriy2003@mail.ru>
2024-03-27 19:45:04 +0500

    com5

--- main.py
+++ main.py
@@ -13,4 +13,18 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
 
+def fib(n):
+   if n <= 2:
+       return 1
+   return fib(n-1)+fib(n-2)
+print(fib(10))
 
+print("Hello Github!!!!!!!!!!!!!!!1")
+
+def func1(x, y):
+    print(x)
+    print(y)
+    return x**2 + y**2
+a = 4
+b = 5
+print(func1(a, b))
